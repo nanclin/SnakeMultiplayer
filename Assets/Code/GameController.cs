56b7a89cc75879e6e81b8b38ea0bdaeaca03bf10
Nejc Anclin <anclin@MacBook-Pro-Anclin.local>
2018-05-27 22:00:01 +0200

    draw history positions

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
     [Header("speed per second")]
     public float Speed = 10;
+    public float TimeInterval = 1;
 
     private float serverDeltaTime = 0;
     private Vector3 ServerPos = Vector3.zero;
+    private List<Vector3> ServerPosList = new List<Vector3>();
 
     private Vector3 ClientPos = Vector3.zero;
+    private List<Vector3> ClientPosList = new List<Vector3>();
     private float ClientAngle = 0;
     private bool Running;
 
@@ -18,6 +22,8 @@ public class GameController : MonoBehaviour {
             serverDeltaTime = 0;
             ClientPos = Vector3.zero;
             ServerPos = Vector3.zero;
+            ClientPosList.Clear();
+            ServerPosList.Clear();
             ClientAngle = 0;
             Running = true;
         }
@@ -27,22 +33,30 @@ public class GameController : MonoBehaviour {
     }
 
     void OnDrawGizmos() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(ServerPos, 0.1f);
-        Gizmos.color = Color.white;
-        Gizmos.DrawWireSphere(ClientPos, 0.1f);
+        for (int i = 0; i < ClientPosList.Count; i++) {
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireSphere(ClientPosList[i], 0.1f);
+        }
+        for (int i = 0; i < ServerPosList.Count; i++) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(ServerPosList[i], 0.1f);
+        }
     }
 
     private void ServerSnake() {
         serverDeltaTime += Time.deltaTime;
-        if (serverDeltaTime >= 0.5f) {
-            serverDeltaTime -= 30 * 0.0167f;
+        if (serverDeltaTime >= TimeInterval) {
+            serverDeltaTime -= 60 * TimeInterval * 0.0167f;
             Debug.LogFormat("ServerSnake() - {0}", serverDeltaTime);
 
             float ang = ClientAngle + Mathf.PI * 0.5f;
             Vector3 dir = new Vector3(Mathf.Cos(ang), Mathf.Sin(ang), 0);
 
-            ServerPos += dir * Speed * 30 * 0.0167f;
+            ServerPos += dir * Speed * 60 * TimeInterval * 0.0167f;
+            ServerPosList.Add(ServerPos);
+            if (ServerPosList.Count > 10) {
+                ServerPosList.RemoveAt(0);
+            }
         }
     }
 
@@ -58,5 +72,9 @@ public class GameController : MonoBehaviour {
         Vector3 dir = new Vector3(Mathf.Cos(ang), Mathf.Sin(ang), 0);
 
         ClientPos += dir * Speed * Time.deltaTime;
+        ClientPosList.Add(ClientPos);
+        if (ClientPosList.Count > 100) {
+            ClientPosList.RemoveAt(0);
+        }
     }
 }
